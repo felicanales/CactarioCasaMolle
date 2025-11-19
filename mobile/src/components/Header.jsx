98a022b20f69bda1ba8e4f0f189a6aeb3ad771c4
Felipe Canales Daire <felcanales@alumnos.uai.cl>
2025-11-19 17:37:22 -0300

    Fix: Correct spelling of "Cactuario" to "Cactario" in Header component

--- mobile/src/components/Header.jsx
+++ mobile/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div className="header-logo">
           <Image src="/logo.png" alt="Cactuario CasaMolle" width={48} height={48} priority />
         </div>
-        <div className="header-title">Cactuario CasaMolle</div>
+        <div className="header-title">Cactario CasaMolle</div>
       </div>
       <div className="header-cactus">🌵</div>
     </header>
